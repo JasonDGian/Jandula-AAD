d59e52e2e0120a9d0fff68a3a03c7bb067a77522
JasonDGian <jasdavg@gmail.com>
2024-09-29 19:53:33 +0200

    Proyecto plantilla

---
+++ Proyectos/plantilla-mysql/src/main/java/IesJandula/DAM2/plantilla_mysql/PlantillaMysqlApplication.java
@@ -0,0 +1,13 @@
+package IesJandula.DAM2.plantilla_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantillaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantillaMysqlApplication.class, args);
+	}
+
+}
---
+++ Proyectos/plantilla-mysql/src/test/java/IesJandula/DAM2/plantilla_mysql/PlantillaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package IesJandula.DAM2.plantilla_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantillaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
